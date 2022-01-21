4ce33fb3e3087a413cd85a53a4c0156e44300790
ItsThatRandom <ryanwaites18@gmail.com>
2022-01-22 00:43:45 +1100

    Javascript and HTML functionality completed, worth re-doing in the future. Styling TBC.

--- script.js
+++ script.js
@@ -1,32 +1,132 @@
+const displayPrev = document.querySelector("#display h3");
+const displayCurrent = document.querySelector("#display h1");
+const numButtons = Array.from(document.querySelectorAll("button[data-number]"));
+const opButtons = Array.from(document.querySelectorAll("button[data-operator]"));
 
-let num1, num2;
-let operator = "";
+let num1 = null, num2 = null, lastOperator = null;
+
+numButtons.forEach(btn => {
+    btn.addEventListener("click", () => displayAction(btn));
+});
+
+opButtons.forEach(btn => {
+    btn.addEventListener("click", () => selectOperation(btn));
+});
 
 function add(num1, num2) {
-    return num1 + num2;
+    return Math.round((num1 + num2) * 100) / 100;
 }  
 
 function subtract(num1, num2) {
-    return num1 - num2;
+    return Math.round((num1 - num2) * 100) / 100;
 }
 
 function multiply(num1, num2) {
-    num1 * num2;
+    return Math.round((num1 * num2) * 100) / 100;
 }
 
 function divide(num1, num2) {
-    num1 / num2;
+    return Math.round((num1 / num2) * 100) / 100;
 }
 
-function operate(operator, num1, num2) {
-    switch(operator) {
-        case "add":
+function operate(lastOperator, num1, num2) {
+    switch(lastOperator) {
+        case "+":
             return add(num1, num2);
-        case "subtract":
+        case "-":
             return subtract(num1, num2);
-        case "multiply":
+        case "*":
             return multiply(num1, num2);
-        case "divide":
+        case "/":
             return divide(num1, num2);
+    }
+}
+
+function displayAction(btn) {
+    displayCurrent.textContent += btn.dataset.number;
+}
+
+function displayOperation(updated, cleared) {
+    num2 = Number(displayCurrent.textContent);
+    num1 = operate(lastOperator, num1, num2);
+    updated.textContent = num1;
+    if (cleared == displayPrev) {
+        cleared.textContent = "";
+    }
+    else {
+        cleared.textContent = "";
+    }
+}
+
+function selectOperation(btn) {
+    switch(btn.dataset.operator) {
+        case "=":
+            if (infiniteCheck()) {
+                return;
+            }
+            result();
+            break;
+        
+        case "Del":
+            if (displayCurrent.textContent.length <= 1) {
+                displayCurrent.textContent = "";
+            }
+            else {
+                displayCurrent.textContent = displayCurrent.textContent.slice(0, -1);
+            }
+            break;
+
+        case "C":
+            clear();
+            break;
+        
+        default:
+            generalOperation(btn);       
+    }
+}
+
+function result() {
+    if (num1 == null || displayCurrent.textContent == "") {
+        return;
+    }
+
+    displayOperation(displayCurrent, displayPrev);
+    num1 = null, num2 = null;
+}
+
+function clear() {
+    displayPrev.textContent = "";
+    displayCurrent.textContent = "";
+    lastOperator = null, num1 = null, num2 = null;
+}
+
+function generalOperation(btn) {
+    if (displayCurrent.textContent == "" && num1 != null) {
+        lastOperator = btn.dataset.operator;
+        displayPrev.textContent = displayPrev.textContent.slice(0, -1) + lastOperator;
+        return;
+    }
+
+    if (num1 == null) {
+        num1 = Number(displayCurrent.textContent);
+        lastOperator = btn.dataset.operator;
+        displayCurrent.textContent = "";
+        displayPrev.textContent = num1 + lastOperator;
+    }
+    
+    else {
+        if (infiniteCheck()) {
+            return;
+        }
+        displayOperation(displayPrev, displayCurrent);
+        lastOperator = btn.dataset.operator;
+        displayPrev.textContent += lastOperator;
+    }
+}
+
+function infiniteCheck() {
+    if (num2 = Number(displayCurrent.textContent) == 0 && lastOperator == "/") {
+        alert("That's not happening, try a different number...");
+        return true;
     }
 }
